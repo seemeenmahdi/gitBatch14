5f04c1b2447b23dbd15ecdc5ad71166a455d4e00
seemeenmahdi <seemeen.mahdi@gmail.com>
2022-09-27 19:57:21 -0400

    second commit

--- src/class03Batch14/class03.java
+++ src/class03Batch14/class03.java
@@ -1,5 +1,11 @@
 package class03Batch14;
 
 public class class03 {
+ public static void main(String[] args) {
+	 
+	System.out.println("hey this is a git class");
+	System.out.println("hey this is a change in my project");
+	System.out.println("hi ");
+ }
 
 }
